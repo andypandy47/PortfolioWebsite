a5a95d3a6e25a6bb807b6671f2bf1c632a73f77d
AndyPandy <akayaudio@gmail.com>
2019-10-12 16:48:54 +0100

    Landing nav added, contact forms changed to info

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,7 @@ const menuBranding = document.querySelector(".menu-branding");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
 const navLinks = document.querySelectorAll(".nav-link");
+const landingNavLinks = document.querySelectorAll(".landing-nav-link");
 
 //Set initial state of menu
 let showMenu = false;
@@ -42,6 +43,15 @@ $(document).ready(function() {
       1000
     );
     toggleMenu();
+  });
+
+  $(landingNavLinks).on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top
+      },
+      1000
+    );
   });
 
   $("#up").on("click", function() {
